cd5cc33afd83a5d0bd736592ce2f7f6c3410e190
ALANDEWI <1599138016@qq.com>
2023-07-12 16:25:58 +0800

    update django extension

--- webmedicine/webmedicine/settings.py
+++ webmedicine/webmedicine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wuz!5&$f0drkl8tap6i)91+pcdw!n*3%==c+^qh9)28j0d4!1^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'webmedicine.urls'
